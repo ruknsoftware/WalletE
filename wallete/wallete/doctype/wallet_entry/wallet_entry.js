4c61e2b80808dbca3007cc08884b6a04967599a9
hamzaabadah <abadahhamza@gmail.com>
2024-07-17 11:14:58 +0300

    feat(Wallet Entry): update transaction_from and validate wallet field query

--- wallete/wallete/doctype/wallet_entry/wallet_entry.js
+++ wallete/wallete/doctype/wallet_entry/wallet_entry.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Wallet Entry', {
-    // refresh: function (frm) {
-    //
-    // }
+	mode_of_payment: function(frm) {
+        if (frm.doc.transaction_type === "Wallet Transfer"){
+            frm.set_query("wallet", ()=> {
+                return {
+                    filters: [
+                        ['name', '!=', frm.doc.mode_of_payment],
+                    ]
+                };
+            });
+        }
+
+	},
+    transaction_type: function (frm){
+        if (frm.doc.transaction_type === "Wallet Payment"){
+            frm.doc.transaction_from = "Mode of Payment";
+        }else if (frm.doc.transaction_type === "Wallet Transfer"){
+            frm.doc.transaction_from = "Wallet";
+        }
+    }
 });
