5e88450805a6ef8bb4b31254b1cb32772d00c5b7
hamzaabadah <abadahhamza@gmail.com>
2024-07-17 15:38:11 +0300

    feat(Wallet Entry): add button to open General Ledger report

--- wallete/wallete/doctype/wallet_entry/wallet_entry.js
+++ wallete/wallete/doctype/wallet_entry/wallet_entry.js
@@ -2,6 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Wallet Entry', {
+    refresh: function (frm){
+        if(frm.doc.docstatus > 0) {
+			frm.add_custom_button(__('Ledger'), function() {
+				frappe.route_options = {
+					"voucher_no": frm.doc.name,
+					"from_date": frm.doc.posting_date,
+					"to_date": moment(frm.doc.modified).format('YYYY-MM-DD'),
+					"company": frm.doc.company,
+					"group_by": '',
+					"show_cancelled_entries": frm.doc.docstatus === 2
+				};
+				frappe.set_route("query-report", "General Ledger");
+			});
+		}
+    },
 	mode_of_payment: function(frm) {
         if (frm.doc.transaction_type === "Wallet Transfer"){
             frm.set_query("wallet", ()=> {
