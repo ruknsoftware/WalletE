e54573553e92b802245ab7f25c9f8cd5e4fdda32
hamzaabadah <abadahhamza@gmail.com>
2024-08-04 14:40:54 +0300

    feat(Wallet): calculate customer wallet

--- wallete/wallete/doctype/wallet/wallet.py
+++ wallete/wallete/doctype/wallet/wallet.py
@@ -3,7 +3,22 @@
 import frappe
 # import frappe
 from frappe.model.document import Document
+from erpnext.accounts.utils import get_balance_on
 
 
 class Wallet(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_customer_wallet(customer):
+	try:
+		customer_wallet = frappe.get_doc("Wallet", {'customer': customer})
+		return get_balance_on(
+			account=customer_wallet.account,
+			party_type="Customer",
+			party=customer_wallet.customer
+		)
+	except frappe.DoesNotExistError:
+		return 0.0
+
