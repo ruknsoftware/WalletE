be086b3a823234a6070072e9043e12b204e4edbe
hamzaabadah <abadahhamza@gmail.com>
2024-08-27 16:28:56 +0300

    feat(Gl Entry): validate customer wallet using get wallet balance

--- wallete/doc_events/gl_entry.py
+++ wallete/doc_events/gl_entry.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import throw, _
-from erpnext.accounts.utils import get_balance_on
+from wallete.wallete.doctype.wallet.wallet import get_customer_wallet_balance
 
 
 def check_customer_wallet_account(doc, method):
@@ -8,10 +8,6 @@ def check_customer_wallet_account(doc, method):
         customer_wallet_account = frappe.get_value("Wallet", {'customer': doc.party}, 'account')
 
         if doc.account == customer_wallet_account:
-            customer_wallet = get_balance_on(
-                account=doc.account,
-                party_type="Customer",
-                party=doc.party
-            )
+            customer_wallet = get_customer_wallet_balance(customer=doc.party)
             if customer_wallet < 0:
                 throw(_("Wallet account for a customer not allowed to be less than zero"))
