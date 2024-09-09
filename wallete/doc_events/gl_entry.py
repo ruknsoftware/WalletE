dd42f3fb1c23711c10a9f2d91149c6d938954a06
hamzaabadah <abadahhamza@gmail.com>
2024-09-09 16:28:14 +0300

    feat(Gl Entry): edit pre-commit

--- wallete/doc_events/gl_entry.py
+++ wallete/doc_events/gl_entry.py
@@ -1,15 +1,15 @@
 import frappe
-from frappe import throw, _
+from frappe import _, throw
+
 from wallete.wallete.doctype.wallet.wallet import get_customer_wallet_balance
 
 
 def check_customer_wallet_account(doc, method):
-    if doc.party_type == "Customer" and doc.party:
-        customer_wallet_account = frappe.get_value(
-            "Wallet", {"customer": doc.party}, "account"
-        )
 
-        if doc.account == customer_wallet_account:
-            customer_wallet = get_customer_wallet_balance(customer=doc.party)
-            if customer_wallet < 0:
-                throw(_("Wallet account for a customer not allowed to be less than zero"))
+	if doc.party_type == "Customer" and doc.party:
+		customer_wallet_account = frappe.get_value("Wallet", {"customer": doc.party}, "account")
+
+		if doc.account == customer_wallet_account:
+			customer_wallet = get_customer_wallet_balance(customer=doc.party)
+			if customer_wallet < 0:
+				throw(_("Wallet account for a customer not allowed to be less than zero"))
