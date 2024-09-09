a6bf97d94fc19a47a8ce3a2bee0da5823deaf3e9
hamzaabadah <abadahhamza@gmail.com>
2024-09-09 16:23:26 +0300

    feat(POS Invoice): reformate black

--- wallete/doc_events/pos_invoice.py
+++ wallete/doc_events/pos_invoice.py
@@ -1,13 +1,14 @@
-from frappe import throw, _
+from frappe import _, throw
+
 from wallete.wallete.doctype.wallet.wallet import (
-    get_customer_wallet_balance,
-    get_wallet_amount_from_payments,
+	get_customer_wallet_balance,
+	get_wallet_amount_from_payments,
 )
 
 
 def override_on_submit(doc, method):
-    wallet_amount = get_wallet_amount_from_payments(doc.payments)
-    customer_wallet = get_customer_wallet_balance(doc.customer, doc.name)
+	wallet_amount = get_wallet_amount_from_payments(doc.payments)
+	customer_wallet = get_customer_wallet_balance(doc.customer, doc.name)
 
-    if wallet_amount > customer_wallet:
-        throw(_("Customer Wallet Balance Must grater than or equal paid from amount"))
+	if wallet_amount > customer_wallet:
+		throw(_("Customer Wallet Balance Must grater than or equal paid from amount"))
