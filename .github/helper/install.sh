0646ea42a3f73a0aa51f35dc6d7849ab4fbdf6a6
HAMZA ABUABADA <abadahhamza@gmail.com>
2024-12-01 16:31:49 +0200

    feat(install): setup branch to clone

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -11,6 +11,8 @@ sudo apt install libcups2-dev redis-server mariadb-client-10.6
 
 pip install frappe-bench
 
+BRANCH_TO_CLONE="develop"
+
 git clone https://github.com/frappe/frappe --branch "$BRANCH_TO_CLONE" --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 
@@ -42,7 +44,7 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app payments --branch ${BRANCH_TO_CLONE%"-hotfix"}
+bench get-app payments --branch "$BRANCH_TO_CLONE"
 bench get-app erpnext --branch "$BRANCH_TO_CLONE"
 bench get-app wallete "${GITHUB_WORKSPACE}"
 bench setup requirements --dev
